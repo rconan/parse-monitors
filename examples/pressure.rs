020f76a46b5229a007d5d492b7e37e1ab71f2f34
Rod Conan <rconan@gmto.org>
2021-10-30 23:25:34 +0000

    ADD GEOTANS TO PRESSURE

--- examples/pressure.rs
+++ examples/pressure.rs
@@ -8,22 +8,46 @@ use std::time::Instant;
 fn main() -> Result<(), Box<dyn Error>> {
     let paths = glob("/fsx/Baseline2021/Baseline2021/Baseline2021/CASES/zen30az000_OS7/pressures/M1p_M1p_*.csv.bz2")?;
     let files: Vec<_> = paths
+        .take(1)
         .map(|p| p.unwrap().to_str().unwrap().to_string())
         .collect();
+    println!("Pressure files: {:?}", files);
+    /*
+        let now = Instant::now();
+        let total_absolute_force: Vec<_> = files
+            .iter()
+            // .progress_count(files.len() as u64)
+            .map(|f| {
+                let pressures = Pressure::load(f).unwrap();
+                pressures.total_absolute_force()
+            })
+            .collect();
+        println!("Elapsed time: {}ms", now.elapsed().as_millis());
+        println!("Total force: {:?}", total_absolute_force);
+    */
     let now = Instant::now();
-    let total_absolute_force: Vec<_> = files
-        .par_iter()
-        .progress_count(files.len() as u64)
-        .map(|f| {
-            let pressures = Pressure::load(f).unwrap();
-            pressures.total_absolute_force()
-        })
-        .collect();
+    let mut pressures = Pressure::load(&files[0]).unwrap();
+    let segments_force = pressures.segments_force();
     println!("Elapsed time: {}ms", now.elapsed().as_millis());
 
-    /*   pressures
-            .x_iter()
-            .zip(pressures.y_iter())
+    println!("x range: {:?}", pressures.x_range());
+    println!("y range: {:?}", pressures.y_range());
+    println!("z range: {:?}", pressures.z_range());
+
+    println!("Segments force: {:?}", segments_force);
+    println!("Total force: {:?}", segments_force.into_iter().sum::<f64>());
+    println!("Total force: {:?}", pressures.total_force());
+    /*
+        &pressures
+            .to_local(7)
+            .xy_iter()
+            .filter_map(|(x, y)| {
+                if x.hypot(*y) < 4.5_f64 {
+                    Some((x, y))
+                } else {
+                    None
+                }
+            })
             .map(|(x, y)| (*x, vec![*y]))
             .collect::<complot::Scatter>();
     */
--- src/pressure.rs
+++ src/pressure.rs
@@ -10,7 +10,7 @@ use bzip2::bufread::BzDecoder;
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
-pub struct Record {
+struct Record {
     #[serde(rename = "Area: Magnitude (m^2)")]
     area: f64,
     #[serde(rename = "Pressure (Pa)")]
@@ -23,16 +23,23 @@ pub struct Record {
     z: f64,
 }
 
+/// M1 segments surface pressure
 #[derive(Default)]
 pub struct Pressure {
-    area: Vec<f64>,
+    /// the segment surface pressure
     pressure: Vec<f64>,
+    /// the area the pressure is applied to
+    area: Vec<f64>,
+    /// the x coordinate where the pressure is applied
     x: Vec<f64>,
+    /// the y coordinate where the pressure is applied
     y: Vec<f64>,
+    /// the z coordinate where the pressure is applied
     z: Vec<f64>,
 }
 
 impl Pressure {
+    /// Loads the pressure from a csv bz2-compressed file
     pub fn load<S: Into<String>>(path: S) -> Result<Self, Box<dyn Error>> {
         let csv_file = File::open(Path::new(&path.into()))?;
         log::info!("Loading {:?}...", csv_file);
@@ -54,20 +61,112 @@ impl Pressure {
         log::info!("... loaded in {:}s", now.elapsed().as_secs());
         Ok(pressure)
     }
+    /// Iterator over the x coordinate
     pub fn x_iter(&self) -> impl Iterator<Item = &f64> {
         self.x.iter()
     }
+    /// Returns the range of the x cooordinate
+    pub fn x_range(&self) -> (f64, f64) {
+        (
+            self.x.iter().cloned().fold(std::f64::INFINITY, f64::min),
+            self.x
+                .iter()
+                .cloned()
+                .fold(std::f64::NEG_INFINITY, f64::max),
+        )
+    }
+    /// Iterator over the y coordinate
     pub fn y_iter(&self) -> impl Iterator<Item = &f64> {
         self.y.iter()
     }
+    /// Iterator over the (x,y) coordinates
+    pub fn xy_iter(&self) -> impl Iterator<Item = (&f64, &f64)> {
+        self.x.iter().zip(self.y.iter())
+    }
+    /// Returns the range of the y cooordinate
+    pub fn y_range(&self) -> (f64, f64) {
+        (
+            self.y.iter().cloned().fold(std::f64::INFINITY, f64::min),
+            self.y
+                .iter()
+                .cloned()
+                .fold(std::f64::NEG_INFINITY, f64::max),
+        )
+    }
+    /// Iterator over the z coordinate
     pub fn z_iter(&self) -> impl Iterator<Item = &f64> {
         self.z.iter()
     }
-    pub fn total_absolute_force(&self) -> f64 {
+    /// Returns the range of the z cooordinate
+    pub fn z_range(&self) -> (f64, f64) {
+        (
+            self.z.iter().cloned().fold(std::f64::INFINITY, f64::min),
+            self.z
+                .iter()
+                .cloned()
+                .fold(std::f64::NEG_INFINITY, f64::max),
+        )
+    }
+    /// Transforms the coordinates into the segment local coordinate system
+    pub fn to_local(&mut self, sid: usize) -> &mut Self {
+        self.x
+            .iter_mut()
+            .zip(self.y.iter_mut().zip(self.z.iter_mut()))
+            .for_each(|(x, (y, z))| {
+                let v: Vec<f64> = geotrans::m1_any_to_oss(sid, [*x, *y, *z]).into();
+                *x = v[0];
+                *y = v[1];
+                *z = v[2];
+            });
+        self
+    }
+    /// Transforms the coordinates into the OSS
+    pub fn from_local(&mut self, sid: usize) -> &mut Self {
+        self.x
+            .iter_mut()
+            .zip(self.y.iter_mut().zip(self.z.iter_mut()))
+            .for_each(|(x, (y, z))| {
+                let v: Vec<f64> = geotrans::oss_to_any_m1(sid, [*x, *y, *z]).into();
+                *x = v[0];
+                *y = v[1];
+                *z = v[2];
+            });
+        self
+    }
+    /// Returns the sum of the z forces of all the segments
+    pub fn total_force(&self) -> f64 {
         self.pressure
             .iter()
             .zip(self.area.iter())
-            .map(|(p, a)| p * a)
+            .map(|(p, a)| -p * a)
             .sum()
+    }
+    /// Iterator over the pressures and areas
+    pub fn pa_iter(&self) -> impl Iterator<Item = (&f64, &f64)> {
+        self.pressure.iter().zip(self.area.iter())
+    }
+    /// Returns the z forces of a given segment
+    pub fn forces(&mut self, sid: usize) -> Vec<f64> {
+        let xy: Vec<_> = self
+            .to_local(sid)
+            .xy_iter()
+            .map(|(x, y)| (*x, *y))
+            .collect();
+        self.from_local(sid)
+            .pa_iter()
+            .zip(xy)
+            .filter(|(_, (x, y))| x.hypot(*y) < 4.5_f64)
+            .map(|((p, a), _)| -p * a)
+            .collect()
+    }
+    /// Returns the sum of the z forces of a given segment
+    pub fn segment_force(&mut self, sid: usize) -> f64 {
+        self.forces(sid).into_iter().sum()
+    }
+    /// Returns the sum of the z forces of a all the segments
+    pub fn segments_force(&mut self) -> Vec<f64> {
+        (1..=7)
+            .map(|sid| self.forces(sid).into_iter().sum())
+            .collect()
     }
 }
