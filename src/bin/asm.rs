dd42f0da2bd7d0fdf0897e6fff5663abada2a2fe
Rod Conan <rconan@gmto.org>
2021-12-21 23:20:01 +0000

    UPDATE ASM PRESSURE: SPLIT INTO 12

--- src/bin/asm.rs
+++ src/bin/asm.rs
@@ -2,107 +2,106 @@
 //use indicatif::ParallelProgressIterator;
 use parse_monitors::{cfd, pressure::Pressure, FORCE_SAMPLING_FREQUENCY};
 use rayon::prelude::*;
-use std::{path::Path, time::Instant};
+use std::{env, iter::once, path::Path, time::Instant};
 
 const R: f64 = 1.2;
 
 fn main() -> anyhow::Result<()> {
-    let duration = 400;
+    let duration = 1;
+
+    let job_idx = env::var("AWS_BATCH_JOB_ARRAY_INDEX")
+        .expect("AWS_BATCH_JOB_ARRAY_INDEX env var missing")
+        .parse::<usize>()
+        .expect("AWS_BATCH_JOB_ARRAY_INDEX parsing failed");
+
+    let cfd_case = cfd::Baseline::<2021>::default()
+        .into_iter()
+        .nth(job_idx)
+        .unwrap();
+
     type M12 = geotrans::M2;
     //let pattern = "M2p_M2p_*.csv.bz2";
     let geometry = "M2p.csv.bz2";
-    let pressure_stats = "m2_pressure-stats_within.csv";
-    cfd::Baseline::<2021>::at_zenith(cfd::ZenithAngle::Thirty)
-        .into_iter()
-        .collect::<Vec<cfd::CfdCase<2021>>>()
+    let pressure_stats = "m2-es_pressure-stats.csv";
+
+    let now = Instant::now();
+    let case_path = cfd::Baseline::<2021>::path().join(cfd_case.to_string());
+    let files: Vec<_> = cfd::CfdDataFile::<2021>::M2Pressure
+        .glob(cfd_case)
+        .unwrap()
+        .map(|p| p.unwrap().to_str().unwrap().to_string())
+        .collect();
+    let n_sample = duration * FORCE_SAMPLING_FREQUENCY as usize;
+    let n_skip = if files.len() < n_sample {
+        panic!("Not enough data sample")
+    } else {
+        files.len() - n_sample
+    };
+
+    let records: Vec<_> = files
         .into_par_iter()
-        .for_each(|cfd_case| {
-            let now = Instant::now();
-            let case_path = cfd::Baseline::<2021>::path().join(cfd_case.to_string());
-            let files: Vec<_> = cfd::CfdDataFile::<2021>::M2Pressure
-                .glob(cfd_case)
+        .skip(n_skip)
+        //.progress_count(n_files as u64)
+        .map(|file| {
+            let path = Path::new(&file);
+            let stem = Path::new(path.file_stem().unwrap())
+                .file_stem()
                 .unwrap()
-                .map(|p| p.unwrap().to_str().unwrap().to_string())
-                .collect();
-            let n_sample = duration * FORCE_SAMPLING_FREQUENCY as usize;
-            let n_skip = if files.len() < n_sample {
-                panic!("Not enough data sample")
-            } else {
-                files.len() - n_sample
-            };
-
-            let records: Vec<_> = files
-                .into_par_iter()
-                .skip(n_skip)
-                //.progress_count(n_files as u64)
-                .map(|file| {
-                    let path = Path::new(&file);
-                    let stem = Path::new(path.file_stem().unwrap())
-                        .file_stem()
-                        .unwrap()
-                        .to_str()
-                        .unwrap();
-                    let time = &stem[8..].parse::<f64>().unwrap();
-                    let csv_pressure = Pressure::<M12>::decompress(path.to_path_buf()).unwrap();
-                    let csv_geometry =
-                        Pressure::<M12>::decompress(path.with_file_name(geometry)).unwrap();
-                    let pressures = Pressure::<M12>::load(csv_pressure, csv_geometry).unwrap();
-
-                    let filter = pressures
-                        .local_radial_filter(1, Some(0.5), None)
-                        .zip(pressures.local_radial_filter(2, Some(0.5), None))
-                        .map(|(l, r)| l || r)
-                        .zip(pressures.local_radial_filter(3, Some(0.5), None))
-                        .map(|(l, r)| l || r)
-                        .zip(pressures.local_radial_filter(4, Some(0.5), None))
-                        .map(|(l, r)| l || r)
-                        .zip(pressures.local_radial_filter(5, Some(0.5), None))
-                        .map(|(l, r)| l || r)
-                        .zip(pressures.local_radial_filter(6, Some(0.5), None))
-                        .map(|(l, r)| l || r)
-                        .zip(pressures.local_radial_filter(7, Some(0.5), None))
-                        .map(|(l, r)| l || r)
-                        .zip(pressures.local_radial_filter(7, None, Some(R)))
-                        .map(|(l, r)| l && r);
-                    let pressure_mean = pressures.mirror_average_pressure_by(filter);
-                    let filter = pressures
-                        .local_radial_filter(1, Some(0.5), None)
-                        .zip(pressures.local_radial_filter(2, Some(0.5), None))
-                        .map(|(l, r)| l || r)
-                        .zip(pressures.local_radial_filter(3, Some(0.5), None))
-                        .map(|(l, r)| l || r)
-                        .zip(pressures.local_radial_filter(4, Some(0.5), None))
-                        .map(|(l, r)| l || r)
-                        .zip(pressures.local_radial_filter(5, Some(0.5), None))
-                        .map(|(l, r)| l || r)
-                        .zip(pressures.local_radial_filter(6, Some(0.5), None))
-                        .map(|(l, r)| l || r)
-                        .zip(pressures.local_radial_filter(7, Some(0.5), None))
-                        .map(|(l, r)| l || r)
-                        .zip(pressures.local_radial_filter(7, None, Some(R)))
-                        .map(|(l, r)| l && r);
-                    let pressure_var =
-                        pressures.mirror_average_pressure_var_by(pressure_mean, filter);
-                    (*time, pressure_mean, pressure_var)
-                })
-                .collect();
-
-            let filename = case_path.join(pressure_stats);
-            let mut wtr = csv::WriterBuilder::new()
-                .has_headers(false)
-                .from_path(filename)
+                .to_str()
                 .unwrap();
-            let headers = [
-                "Time [s]".to_string(),
-                "Mean [Pa]".to_string(),
-                "Var [Pa]".to_string(),
-            ];
-            wtr.write_record(&headers).unwrap();
-            for data in records {
-                wtr.serialize(data).unwrap();
+            let time = &stem[8..].parse::<f64>().unwrap();
+            let csv_pressure = Pressure::<M12>::decompress(path.to_path_buf()).unwrap();
+            let csv_geometry = Pressure::<M12>::decompress(path.with_file_name(geometry)).unwrap();
+            let pressures = Pressure::<M12>::load(csv_pressure, csv_geometry).unwrap();
+
+            let mut pressure_mean = Vec::<f64>::new();
+            for i in 0..12 {
+                let filter = pressures
+                    .local_radial_filter(1, Some(0.5), None)
+                    .zip(pressures.local_radial_filter(2, Some(0.5), None))
+                    .map(|(l, r)| l || r)
+                    .zip(pressures.local_radial_filter(3, Some(0.5), None))
+                    .map(|(l, r)| l || r)
+                    .zip(pressures.local_radial_filter(4, Some(0.5), None))
+                    .map(|(l, r)| l || r)
+                    .zip(pressures.local_radial_filter(5, Some(0.5), None))
+                    .map(|(l, r)| l || r)
+                    .zip(pressures.local_radial_filter(6, Some(0.5), None))
+                    .map(|(l, r)| l || r)
+                    .zip(pressures.local_radial_filter(7, Some(0.5), None))
+                    .map(|(l, r)| l || r)
+                    .zip(pressures.local_radial_filter(7, None, Some(R)))
+                    .map(|(l, r)| l && r)
+                    .zip(pressures.xy_iter().map(|(x, y)| {
+                        if i < 6 {
+                            let (s, c) = (90f64 - 60f64 * i as f64).to_radians().sin_cos();
+                            (x - 0.55 * c).hypot(y - 0.55 * s) < 0.05
+                        } else {
+                            let (s, c) = (-60f64 * i as f64).to_radians().sin_cos();
+                            (x - 0.942 * c).hypot(y - 0.942 * s) < 0.05
+                        }
+                    }))
+                    .map(|(l, r)| l && r);
+                pressure_mean.push(pressures.mirror_average_pressure_by(filter));
             }
-            wtr.flush().unwrap();
-            println!("{:<32}: {:>8}s", cfd_case, now.elapsed().as_secs());
-        });
+            (*time, pressure_mean)
+        })
+        .collect();
+
+    let filename = case_path.join(pressure_stats);
+    let mut wtr = csv::WriterBuilder::new()
+        .has_headers(false)
+        .from_path(filename)
+        .unwrap();
+    let headers: Vec<String> = once("Time [s]".to_string())
+        .chain((1..13).map(|i| format!("ES {}", i)))
+        .collect();
+    wtr.write_record(&headers).unwrap();
+    for data in records {
+        wtr.serialize(data).unwrap();
+    }
+    wtr.flush().unwrap();
+    println!("{:<32}: {:>8}s", cfd_case, now.elapsed().as_secs());
+
     Ok(())
 }
