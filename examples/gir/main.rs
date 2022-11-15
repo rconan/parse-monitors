f4c5d29afb01cdaf38d3b6dd45accaac8d32ea32
Rod Conan <rconan@gmto.org>
2022-11-15 01:00:43 +0000

    GIR PRESSURE FOR AN ENTIRE CASE

--- examples/gir/main.rs
+++ examples/gir/main.rs
@@ -1,62 +1,85 @@
-use std::{fs::File, time::Instant};
+use std::fs::File;
 
-use parse_monitors::pressure::{rtree::Node, Telescope};
+use parse_monitors::{
+    cfd::{self, BaselineTrait},
+    pressure::{rtree::Node, Telescope},
+};
 use polars::prelude::*;
 use serde::Serialize;
 
 #[derive(Serialize)]
-pub struct Data<'a> {
+pub struct Data {
     mean_pressure: f64,
-    gir_panels: Vec<Vec<&'a Node>>,
+    gir_panels: Vec<Vec<Node>>,
 }
 
 fn main() -> anyhow::Result<()> {
-    let telescope =
-        Telescope::from_path("../../data/Telescope_p_telescope_7.000000e+02.csv.z").unwrap();
-    println!("{telescope}");
-    let mean_pressure = telescope.mean_pressure();
-    let rtree = telescope.to_rtree();
+    let cfd_case = cfd::Baseline::<2021>::default().into_iter().nth(0).unwrap();
+    let data_file = cfd::CfdDataFile::<2021>::TelescopePressure;
+    let telescope_pressure_files = data_file.glob(cfd_case)?;
 
-    let mut gir_panels = vec![]; // index: 0=>a , 1=>b, 2=>c
+    dbg!(&telescope_pressure_files);
 
-    for gir_csv in ["gir_a.csv", "gir_b.csv", "gir_c.csv"] {
-        let df = CsvReader::from_path(gir_csv)?.has_header(true).finish()?;
-        println!("Dataframe shape: {:?}", df.shape());
-        // println!("{}", df.head(Some(10)));
+    let data = telescope_pressure_files
+        .into_iter()
+        .take(1)
+        .map(|file| {
+            let telescope = Telescope::from_path(file).unwrap();
+            println!("{telescope}");
+            let mean_pressure = telescope.mean_pressure();
+            let rtree = telescope.to_rtree();
 
-        let now = Instant::now();
-        gir_panels.push(
-            df[3]
-                .f64()?
-                .into_iter()
-                .zip(df[4].f64()?)
-                .zip(df[5].f64()?)
-                .filter_map(|xyz| {
-                    if let ((Some(x), Some(y)), Some(z)) = xyz {
-                        rtree.locate_at_point(&[x, y, z])
-                    } else {
-                        None
-                    }
-                })
-                .collect::<Vec<_>>(),
-        );
-        let (n_sample, _) = df.shape();
-        let n_node = gir_panels.last().unwrap().len();
-        println!(
-            "Identified {} nodes out of {} in {}ms",
-            n_node,
-            n_sample,
-            now.elapsed().as_millis()
-        );
+            let mut gir_panels = vec![]; // index: 0=>a , 1=>b, 2=>c
 
-        // dbg!(nodes[0]);
-    }
-    let data = Data {
-        mean_pressure,
-        gir_panels,
-    };
+            for gir_csv in ["gir_a.csv", "gir_b.csv", "gir_c.csv"] {
+                let df = CsvReader::from_path(gir_csv)
+                    .unwrap()
+                    .has_header(true)
+                    .finish()
+                    .unwrap();
+                // println!("Dataframe shape: {:?}", df.shape());
+                // println!("{}", df.head(Some(10)));
+
+                // let now = Instant::now();
+                gir_panels.push(
+                    df[3]
+                        .f64()
+                        .unwrap()
+                        .into_iter()
+                        .zip(df[4].f64().unwrap())
+                        .zip(df[5].f64().unwrap())
+                        .filter_map(|xyz| {
+                            if let ((Some(x), Some(y)), Some(z)) = xyz {
+                                rtree.locate_at_point(&[x, y, z]).map(|node| node.clone())
+                            } else {
+                                None
+                            }
+                        })
+                        .collect::<Vec<_>>(),
+                );
+                // let (n_sample, _) = df.shape();
+                // let n_node = gir_panels.last().unwrap().len();
+                /*                 println!(
+                    "Identified {} nodes out of {} in {}ms",
+                    n_node,
+                    n_sample,
+                    now.elapsed().as_millis()
+                ); */
+
+                // dbg!(nodes[0]);
+            }
+            Data {
+                mean_pressure,
+                gir_panels,
+            }
+        })
+        .collect::<Vec<Data>>();
+
+    let cfd_path = cfd::Baseline::<2021>::default_path();
+    let cfd_case_path = cfd_path.join(cfd_case.to_string());
+    dbg!(&cfd_case_path);
     serde_pickle::to_writer(
-        &mut File::create("gir_panels.pkl")?,
+        &mut File::create(cfd_case_path.join("gir_panels.pkl")).unwrap(),
         &data,
         Default::default(),
     )?;
